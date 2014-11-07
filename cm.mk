d9081efaf8ea97f2eb99b733e752840ce3a1a677
andixlm <andixev@gmail.com>
2014-11-07 21:08:53 +0500

    Add build fingerprint / ID / Product Name ect.

--- cm.mk
+++ cm.mk
@@ -17,3 +17,6 @@ PRODUCT_NAME := cm_s2vep
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-I9105P
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=s2vepxx TARGET_DEVICE=s2vep BUILD_FINGERPRINT="samsung/s2vepxx/s2vep:4.2.2/JDQ39/I9105PXXUBNG1:user/release-keys" PRIVATE_BUILD_DESC="s2vepxx-user 4.2.2 JDQ39 I9105PXXUBNG1 release-keys"
