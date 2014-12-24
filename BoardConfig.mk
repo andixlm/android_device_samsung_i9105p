b17f914116e6607eb83cfa69417bd78ed6bd53dd
andixlm <andixev@gmail.com>
2014-12-24 23:24:50 +0500

    Move bdroid_buildcfg.h to common repo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,14 +1,11 @@
 # Include common BoardConfig
 -include device/samsung/galaxys2plus-common/BoardConfigCommon.mk
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9105p/bluetooth
+# Kernel
+TARGET_KERNEL_CONFIG := cyanogenmod_s2vep_defconfig
 
 # NFC
 BOARD_NFC_HAL_SUFFIX := capri
-
-# Kernel
-TARGET_KERNEL_CONFIG := cyanogenmod_s2vep_defconfig
 
 # Recovery
 TARGET_RECOVERY_FSTAB := device/samsung/i9105p/ramdisk/fstab.capri_ss_s2vep
