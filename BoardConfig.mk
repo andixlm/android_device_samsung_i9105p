f7634c66a5636124bbdf3447e6504a7e28e0cfc7
andixlm <andixev@gmail.com>
2014-12-25 18:25:02 +0500

    BoardConfig: resort some lines

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,11 +4,11 @@
 # Kernel
 TARGET_KERNEL_CONFIG := cyanogenmod_s2vep_defconfig
 
-# NFC
-BOARD_NFC_HAL_SUFFIX := capri
-
 # Recovery
 TARGET_RECOVERY_FSTAB := device/samsung/i9105p/ramdisk/fstab.capri_ss_s2vep
+
+# NFC
+BOARD_NFC_HAL_SUFFIX := capri
 
 # SELinux
 BOARD_SEPOLICY_DIRS += \
@@ -17,5 +17,5 @@ BOARD_SEPOLICY_DIRS += \
 BOARD_SEPOLICY_UNION += \
     file_contexts
 
-# Inherit from the proprietary version
+# inherit from the proprietary version
 -include vendor/samsung/galaxys2plus-common/BoardConfigVendor.mk
