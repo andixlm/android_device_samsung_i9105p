6e1605b8769d19250e5f68b7aa7cab84fe5f2ee7
andixlm <andixev@gmail.com>
2015-01-02 12:43:56 +0500

    Pull the latest changes from cm-12.0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,14 +2,13 @@
 -include device/samsung/galaxys2plus-common/BoardConfigCommon.mk
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/samsung/galaxys2plus-common
 TARGET_KERNEL_CONFIG := cyanogenmod_s2vep_defconfig
-
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/i9105p/bluetooth
 
 # Recovery
 TARGET_RECOVERY_FSTAB := device/samsung/i9105p/ramdisk/fstab.capri_ss_s2vep
+
+# NFC
+BOARD_NFC_HAL_SUFFIX := capri
 
 # inherit from the proprietary version
 -include vendor/samsung/galaxys2plus-common/BoardConfigVendor.mk
