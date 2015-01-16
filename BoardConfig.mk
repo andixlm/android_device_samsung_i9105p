db6703a14f7b5904aca4f16fb1943f23435036cb
andixlm <andixev@gmail.com>
2015-01-16 20:02:59 +0500

    Revert "Move TARGET_KERNEL_SOURCE to a common repo"
    This reverts commit 808e31a50ab633fbd8823d113252b68b20e06f8e.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,6 +2,7 @@
 -include device/samsung/galaxys2plus-common/BoardConfigCommon.mk
 
 # Kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/galaxys2plus-common
 TARGET_KERNEL_CONFIG := cyanogenmod_s2vep_defconfig
 
 # Recovery
